36372fc32b95515c38e2a78df27690a70569cd61
UYasher <shriyash.upadhyay@gmail.com>
2018-09-09 08:46:43 -0400

    finished mostly

--- documentation/views.py
+++ documentation/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "documentation/index.html", context={})
+
+def about(request):
+    return render(request, "documentation/about.html", context={})
+
+def how(request):
+    return render(request, "documentation/how.html", context={})
--- kharon_web/urls.py
+++ kharon_web/urls.py
@@ -18,7 +18,9 @@ from django.urls import path, include
 from documentation import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='index'),
+    path('about/', views.about,name='about'),
+    path('how/', views.how, name='how'),
     path('docs/', include('documentation.urls')),
     path('admin/', admin.site.urls),
 ]
